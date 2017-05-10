f98b5fbb2c88f313043b6e34fda1805ca62e35d7
Student <yashharmalkar27@gmail.com>
2017-05-10 12:07:03 -0700

    New commit

--- src/bulb/lightbulbTest.java
+++ src/bulb/lightbulbTest.java
@@ -12,12 +12,14 @@ public class lightbulbTest {
 	@Before
 	public void setUp() throws Exception {
 		mybulb= new lightbulb(1000);
+		System.out.println("ADDED WATTAGE");
 	}
 
 	@Test
 	public void test() {
 		mybulb.power(1000);
 		assertEquals(2000,mybulb.getBulbwattage());
+		System.out.println("ADDED POWER");
 	}
 
 }
